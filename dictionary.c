71df3ead23d7c64eabfb13548d438f1d269e4d16
mzkahane <80354992+mzkahane@users.noreply.github.com>
2025-05-25 18:09:29 -0400

    Add getWord function

--- dictionary.c
+++ dictionary.c
@@ -5,8 +5,10 @@
 
 #define MAX_WORD_LENGTH 6
 
+int wordCount = 0;
+
 WordEntry dictionary[WORD_COUNT];
-int wordCount = 0; // Track actual number of words
+//int wordCount = 0; // Track actual number of words
 
 void populateDictionary(const char *filename) {
 	FILE *file = fopen(filename, "r");
@@ -23,4 +25,28 @@ void populateDictionary(const char *filename) {
 	}
 
 	fclose(file);
+}
+
+char * getWord(int number) {
+	int low = 0;
+	int high = wordCount - 1; //(sizeof(dictionary) / sizeof(dictionary[0])) - 1;
+
+	while (low <= high) {
+		int mid = low + (high - low) / 2;
+
+		if(dictionary[mid].number == number) {
+			return dictionary[mid].word;
+		}
+
+		if (dictionary[mid].number < number) {
+			low = mid + 1;
+		}
+
+		else {
+			high = mid - 1;
+		}
+	}
+
+	printf("No word was found for the dice roll: %d\n", number);
+	return NULL;
 }
--- dictionary.h
+++ dictionary.h
@@ -1,7 +1,7 @@
 #ifndef DICTIONARY_H
 #define DICTIONARY_H
 
-#define WORD_COUNT 1330
+#define WORD_COUNT 1296
 
 typedef struct {
 	char word[5];
@@ -12,5 +12,7 @@ typedef struct {
 extern WordEntry dictionary[WORD_COUNT];
 
 void populateDictionary(const char *filename);
+
+char * getWord(int number);
 
 #endif // DICTIONARY_H
